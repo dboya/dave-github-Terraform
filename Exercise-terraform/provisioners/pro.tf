594afdd581b297f6dd3f3244ed43c8ee5dafbb9f
dboya <94754886+dboya@users.noreply.github.com>
2024-02-17 16:20:05 -0500

    Update pro.tf

--- Exercise-terraform/provisioners/pro.tf
+++ Exercise-terraform/provisioners/pro.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAW7MRKFNNL2UPOUHV"
-  secret_key = "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"
 }
 
 resource "aws_instance" "webserver" {
